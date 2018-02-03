6972ac5114d601982a7ec96e23b3bb315751aa36
dipaliware20 <dipaliware20@gmail.com>
2018-02-03 16:00:42 +0530

    added splash screen image view and imges in drawable

--- app/src/main/java/android/photoapp/shutter/SplashActivity.java
+++ app/src/main/java/android/photoapp/shutter/SplashActivity.java
@@ -2,6 +2,7 @@ package android.photoapp.shutter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
     }
 }
