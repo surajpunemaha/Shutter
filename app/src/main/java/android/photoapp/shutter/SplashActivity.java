8898fd66b540404c75f639b901b0e1a738bf8d92
dipaliware20 <dipaliware20@gmail.com>
2018-02-03 17:40:51 +0530

    updated

--- app/src/main/java/android/photoapp/shutter/SplashActivity.java
+++ app/src/main/java/android/photoapp/shutter/SplashActivity.java
@@ -1,5 +1,6 @@
 package android.photoapp.shutter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -10,6 +11,22 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        final Thread thread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    /*Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);*/
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+        };
+        thread.start();
 
 
     }
