230aced48c0eead74a665898a355e734e9d1bed4
surajpunemaha <surajbang26@gmail.com>
2018-02-15 12:44:34 +0530

    commit by suraj

--- app/src/main/java/android/photoapp/shutter/SignUpActivity.java
+++ app/src/main/java/android/photoapp/shutter/SignUpActivity.java
@@ -6,7 +6,8 @@ import android.os.Bundle;
 public class SignUpActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
     }
